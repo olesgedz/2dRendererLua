bb75f88f76ff9cd3efe1f99c192dca80c1d75411
Oles <gedzoles@gmail.com>
2024-06-21 23:09:45 +0300

    linking

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
+
+int main() {
+    // Simply tries to use the Lua language with the Sol library
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    // Tries to create a vector2 and normalize it with the GLM library
+    glm::vec2 velocity = glm::vec2(5.0, -2.5);
+    velocity = glm::normalize(velocity);
+
+    // Tries to initialize SDL
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    std::cout << "Yay! Dependencies work correctly." << std::endl;
+    return 0;
+}
