7220c83e85cb1da4673b24c848a877b8cc9baf2c
Oles <gedzoles@gmail.com>
2024-06-23 14:47:13 +0300

    skelet

---
+++ src/Game.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Game {
+public:
+    Game();
+
+    void initialize();
+    void run();
+    void processInput();
+    void update();
+    void render();
+    void destroy();
+private:
+    //...
+};
