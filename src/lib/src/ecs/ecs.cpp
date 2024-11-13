1da120faaa1bec68c02d2ba6796bb1dd22f14ce0
Oles <gedzoles@gmail.com>
2024-11-13 23:13:24 +0200

    init tags and groups

---
+++ src/lib/src/ecs/ecs.cpp
@@ -0,0 +1,163 @@
+module;
+
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <memory>
+#include <set>
+#include <string>
+#include <typeindex>
+#include <unordered_map>
+#include <vector>
+
+module ecs;
+
+// System class methods
+void System::addEntity(Entity entity) { _entities.push_back(entity); }
+
+void System::removeEntityFromSystem(Entity entity) {
+  std::erase_if(_entities, [&entity](const Entity& other) { return other == entity; });
+}
+
+// Registry class methods
+Registry::Registry(Registry&& other) noexcept {
+  _numEntities = other._numEntities;
+  _componentPools = std::move(other._componentPools);
+  _entityComponentSignatures = std::move(other._entityComponentSignatures);
+  _systems = std::move(other._systems);
+  _entitiesToBeAdded = std::move(other._entitiesToBeAdded);
+  _entitiesToBeKilled = std::move(other._entitiesToBeKilled);
+}
+
+// copy constructor
+Registry::Registry(const Registry& other) {
+  _numEntities = other._numEntities;
+  _componentPools = other._componentPools;
+  _entityComponentSignatures = other._entityComponentSignatures;
+  _systems = other._systems;
+  _entitiesToBeAdded = other._entitiesToBeAdded;
+  _entitiesToBeKilled = other._entitiesToBeKilled;
+}
+
+// Tag management
+void Registry::tagEntity(Entity entity, const std::string& tag) {
+  entityPerTag.emplace(tag, entity);
+  tagPerEntity.emplace(entity.getId(), tag);
+}
+
+bool Registry::entityHasTag(Entity entity, const std::string& tag) const { return true; }
+
+void Registry::removeEntityTag(Entity entity) {
+  auto taggedEntity = tagPerEntity.find(entity.getId());
+  if (taggedEntity != tagPerEntity.end()) {
+    auto tag = taggedEntity->second;
+    entityPerTag.erase(tag);
+    tagPerEntity.erase(taggedEntity);
+  }
+}
+
+// Group management
+void Registry::groupEntity(Entity entity, const std::string& group) {
+  entitiesPerGroup.emplace(group, std::set<Entity>());
+  entitiesPerGroup[group].emplace(entity);
+  groupPerEntity.emplace(entity.getId(), group);
+}
+
+bool Registry::entityBelongsToGroup(Entity entity, const std::string& group) const {
+  auto groupEntities = entitiesPerGroup.at(group);
+  return groupEntities.find(entity.getId()) != groupEntities.end();
+}
+
+std::vector<Entity> Registry::getEntitiesByGroup(const std::string& group) const {
+  auto& setOfEntities = entitiesPerGroup.find(group)->second;
+  return std::vector<Entity>(setOfEntities.begin(), setOfEntities.end());
+}
+
+void Registry::removeEntityFromGroup(Entity entity) {
+  auto groupedEntity = groupPerEntity.find(entity.getId());
+  if (groupedEntity != groupPerEntity.end()) {
+    auto group = entitiesPerGroup.find(groupedEntity->second);
+    if (group != entitiesPerGroup.end()) {
+      group->second.erase(entity);
+    }
+  }
+  groupPerEntity.erase(groupedEntity);
+}
+
+Entity Registry::createEntity() {
+  size_t entityId;
+  if (_freedIds.empty()) {
+    entityId = _numEntities++;
+
+    if (entityId >= _entityComponentSignatures.size()) {
+      _entityComponentSignatures.resize(entityId + 1);
+    }
+  } else {
+    entityId = _freedIds.front();
+    _freedIds.pop_front();
+  }
+
+  Entity entity(entityId);
+  entity.registry = this;
+  _entitiesToBeAdded.insert(entity);
+  if (entityId >= _entityComponentSignatures.size()) {
+    _entityComponentSignatures.resize(entityId + 1);
+  }
+
+  Logger::log("Entity created with id: " + std::to_string(entityId));
+
+  return entity;
+}
+
+void Registry::killEntity(Entity entity) { _entitiesToBeKilled.insert(entity); }
+
+void Registry::update() {
+  // Actually add/remove  the entities between frames
+  for (const auto& entity : _entitiesToBeAdded) {
+    addEntityToSystem(entity);
+    Logger::log("Entity added to system: " + std::to_string(entity.getId()));
+  }
+  _entitiesToBeAdded.clear();
+
+  for (auto entity : _entitiesToBeKilled) {
+    removeEntityFromSystems(entity);
+
+    _entityComponentSignatures[entity.getId()].reset();
+    // add to freed ids
+    _freedIds.push_back(entity.getId());
+  }
+  _entitiesToBeKilled.clear();
+}
+
+void Registry::addEntityToSystem(Entity entity) {
+  const auto entityId = entity.getId();
+
+  const auto& entitySignature = _entityComponentSignatures[entityId];
+
+  // loop all the systems
+  for (auto& system : _systems) {
+    const auto& systemSignature = system.second->getComponentSignature();
+
+    // Check if the entity signature matches the system signature
+    if ((entitySignature & systemSignature) == systemSignature) {
+      system.second->addEntity(entity);
+    }
+  }
+}
+
+void Registry::removeEntityFromSystems(Entity entity) {
+  for (auto& system : _systems) {
+    system.second->removeEntityFromSystem(entity);
+  }
+}
+
+// Entity class methods
+void Entity::tag(const std::string& tag) { registry->tagEntity(*this, tag); }
+
+bool Entity::hasTag(const std::string& tag) const { return registry->entityHasTag(*this, tag); }
+
+void Entity::group(const std::string& group) { registry->groupEntity(*this, group); }
+
+bool Entity::belongsToGroup(const std::string& group) const { return registry->entityBelongsToGroup(*this, group); }
+
+void Entity::kill() { registry->killEntity(*this); }
