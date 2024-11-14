999c979d2e434c227186704153243d4efb481d92
Oles <gedzoles@gmail.com>
2024-11-14 22:36:16 +0200

    linux rm cause of ninja

--- test/src/registry_tests.cpp
+++ test/src/registry_tests.cpp
@@ -26,3 +26,31 @@ TEST(Registry, RemoveEntity) {
   registry->update();
   EXPECT_EQ(registry->getNumEntities(), 1);
 }
+
+TEST(Registry, AddTag) {
+  // Add entity
+  auto registry = std::make_unique<Registry>();
+  auto entity = registry->createEntity();
+  entity.tag("test_tag");
+  EXPECT_EQ(entity.hasTag("test_tag"), true);
+}
+
+TEST(Registry, RemoveTag) {
+  // Add entity
+  auto registry = std::make_unique<Registry>();
+  auto entity = registry->createEntity();
+  entity.tag("test_tag");
+  EXPECT_EQ(entity.hasTag("test_tag"), true);
+
+  entity.registry->removeEntityTag(entity);
+  EXPECT_EQ(entity.hasTag("test_tag"), false);
+}
+
+TEST(Registry, AddGroup) {
+  // Add entity
+  auto registry = std::make_unique<Registry>();
+  auto entity = registry->createEntity();
+  entity.group("test_group");
+
+  EXPECT_EQ(entity.belongsToGroup("test_group"), true);
+}
