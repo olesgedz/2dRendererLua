337012dcef3a227d718760b7096eac918912ba30
Oles <gedzoles@gmail.com>
2024-11-22 03:03:35 +0200

    tests

---
+++ test/src/pool_tests.cpp
@@ -0,0 +1,78 @@
+#include <gtest/gtest.h>
+
+import pool;
+import components;
+
+template <typename T>
+struct PoolTest {
+  std::unordered_map<int, int>& _entityIdToIndex;
+  std::unordered_map<int, int>& _indexToEntityId;
+  std::vector<T>& _data;
+  int& _size;
+
+  PoolTest(Pool<T>& pool)
+      : _entityIdToIndex(pool._entityIdToIndex),
+        _indexToEntityId(pool._indexToEntityId),
+        _data(pool._data),
+        _size(pool._size) {}
+};
+
+TEST(Pool, isEmpty) {
+  auto pool = std::make_shared<Pool<HealthComponent>>();
+  PoolTest<HealthComponent> poolTest(*pool);
+
+  EXPECT_EQ(pool->getSize(), 0);
+  EXPECT_EQ(poolTest._size, 0);
+
+  int entityId = 0;
+  pool->set(entityId, HealthComponent(100));
+  EXPECT_NE(pool->getSize(), 0);
+  EXPECT_NE(poolTest._size, 0);
+}
+
+TEST(Pool, AddEntity) {
+  auto pool = std::make_shared<Pool<HealthComponent>>();
+
+  EXPECT_EQ(pool->getSize(), 0);
+
+  int entityId = 0;
+  pool->set(entityId, HealthComponent(100));
+  EXPECT_EQ(pool->getSize(), 1);
+
+  pool->removeFromPull(entityId);
+  EXPECT_EQ(pool->getSize(), 0);
+}
+
+TEST(Pool, RemoveEntity) {
+  auto pool = std::make_shared<Pool<HealthComponent>>();
+  PoolTest<HealthComponent> poolTest(*pool);
+
+  int entityId = 0;
+  int entityId2 = 1;
+
+  pool->set(entityId, HealthComponent(100));
+  pool->set(entityId2, HealthComponent(102));
+
+  EXPECT_EQ(pool->getSize(), 2);
+  EXPECT_EQ(poolTest._entityIdToIndex[entityId2], 1);
+  EXPECT_EQ(poolTest._entityIdToIndex[entityId], 0);
+
+  pool->removeFromPull(entityId2);
+  EXPECT_EQ(poolTest._entityIdToIndex[entityId2], 0);
+}
+
+TEST(Pool, SetEntity) {
+  auto pool = std::make_shared<Pool<HealthComponent>>();
+  PoolTest<HealthComponent> poolTest(*pool);
+
+  int entityId = 0;
+
+  pool->set(entityId, HealthComponent(100));
+
+  EXPECT_EQ(pool->getSize(), 1);
+  EXPECT_EQ(pool->get(entityId).healthPrecentage, 100);
+
+  pool->set(entityId, HealthComponent(42));
+  EXPECT_EQ(pool->getSize(), 1);
+  EXPECT_EQ(pool->get(entityId).healthPrecentage, 42);
+}
--- test/src/registry_tests.cpp
+++ test/src/registry_tests.cpp
@@ -1,8 +1,39 @@
 #include <deque>
+#include <glm/gtc/constants.hpp>
 
 #include "gtest/gtest.h"
 
 import ecs;
+import components;
+
+class RegistryTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    registry = std::make_unique<Registry>();
+    std::cout << "Setup" << std::endl;
+  }
+
+  void TearDown() override {
+    registry.reset();
+    std::cout << "TearDown" << std::endl;
+  }
+
+  std::unique_ptr<Registry> registry;
+};
+
+TEST_F(RegistryTest, AddComponent) {
+  auto entity = registry->createEntity();
+  entity.addComponent<HealthComponent>(100);
+  EXPECT_EQ(entity.hasComponent<HealthComponent>(), true);
+}
+
+TEST_F(RegistryTest, RemoveComponent) {
+  auto entity = registry->createEntity();
+  entity.addComponent<HealthComponent>(100);
+  EXPECT_EQ(entity.hasComponent<HealthComponent>(), true);
+  entity.removeComponent<HealthComponent>();
+  EXPECT_EQ(entity.hasComponent<HealthComponent>(), false);
+}
 
 TEST(Registry, AddEntity) {
   auto registry = std::make_unique<Registry>();
@@ -14,6 +45,22 @@ TEST(Registry, AddEntity) {
 }
 
 TEST(Registry, RemoveEntity) {
+  // Add entity
+  auto registry = std::make_unique<Registry>();
+  auto entity = registry->createEntity();
+  EXPECT_EQ(entity.getId(), 0);
+  EXPECT_EQ(registry->getNumEntities(), 1);
+  registry->update();
+  EXPECT_EQ(registry->getNumEntities(), 1);
+
+  // Remove entity
+  entity.kill();
+  EXPECT_EQ(registry->getNumEntities(), 1);
+  registry->update();
+  EXPECT_EQ(registry->getNumEntities(), 1);
+}
+
+TEST_F(RegistryTest, addComponent) {
   // Add entity
   auto registry = std::make_unique<Registry>();
   auto entity = registry->createEntity();
