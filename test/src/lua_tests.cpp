82bc76b339438786e04ac84b45cb6497560175e3
Oles <gedzoles@gmail.com>
2024-12-23 15:01:09 +0200

    lua scripting init

--- test/src/lua_tests.cpp
+++ test/src/lua_tests.cpp
@@ -17,3 +17,16 @@ TEST(Lua, SimpleTest) {
   int result = functionFactorial(5);
   EXPECT_EQ(result, 120);
 }
+
+int cppFunction() {
+  std::cout << "Hello from C++!" << std::endl;
+  return 42;
+}
+
+TEST(Lua, CPPFunction) {
+  sol::state lua;
+
+  lua["cppFunction"] = cppFunction;
+  lua.open_libraries(sol::lib::base);
+  lua.script_file("../assets/scripts/simple_test.lua");
+}
